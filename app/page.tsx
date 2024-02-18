f386648cb1826f60ac90a506638305dc94cc91bc
CrossStack-Q <crossstack@gmail.com>
2024-02-18 23:41:28 +0530

    Auth Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import Loading from "@/components/auth/loading";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/clerk-react";
 
 export default function Home() {
   return (
     <main>
       <Button variant={"destructive"} size={"lg"}>
-        Hello ji
+        This is screen for Authenticated users only .
       </Button>
+      <div>
+        <UserButton/>
+      </div>
     </main>
   );
 }
